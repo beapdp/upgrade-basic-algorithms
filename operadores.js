0262b4b986aff0cb00816c0605665bffbdb3590c
i-am-iron-man <iron-man@avengers.com>
2022-10-19 23:23:46 +0200

    iteration#3

---
+++ operadores.js
@@ -0,0 +1,28 @@
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+let num1 = 5;
+let num2 = 10;
+console.log(num1*num2);
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+let number1 = 10;
+let number2 = 2;
+console.log(number1 / number2);
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+let div = 15;
+let divsor = 9;
+let resto = num1 % num2;
+console.log(resto);
+
+
+
+/*1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+teniendo dos variables y = 10 y z = 5.*/
+let y = 10;
+let z = 5;
+let x = y + z;
+console.log(x)
+/*1.5 Usa el correcto operador de asignación que resultará en x = 50,
+teniendo dos variables y = 10 y z = 5.*/
+let mult = y * z;
+console.log(mult);
--- variables-avanzadas.js
+++ variables-avanzadas.js
@@ -6,9 +6,12 @@ console.log('Soy '+ firstName + ' ' + lastName + ', tengo ' + age +  'años y me
 
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
-console.log(toy1.price + toy2.price)
+console.log(toy1.price + toy2)
 
 
 let globalBasePrice = 25000;
-const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: basePrice + globalBasePrice};
-const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: basePrice + globalBasePrice};
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+car1.finalPrice = globalBasePrice + car1.basePrice;
+car2.finalPrice = globalBasePrice + car2.basePrice;
